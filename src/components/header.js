e8ad2c9676c2bfb73eb087c3fe1845610719d2b7
Ellis Delgado <ellisdelgado13@gmail.com>
2021-05-11 15:16:42 -0700

    added filter for  news

--- src/components/header.js
+++ src/components/header.js
@@ -1,47 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/header.css'
 
 
-class Header extends Component {
-
-    state = {
-        active: false,
-        keywords: '',
+const Header = (props) => {
 
 
-    }
+    return (
 
-    inputChangeHandler = (event) => {
-        // console.log(event.target.value)
-        const value = event.target.value === '' ? 'not-active' : 'active';
-
-        this.setState({
-            active: value,
-            keywords: event.target.value
-        })
-
-    }
-
-
-    render() {
-
-
-
-        console.log(this.state)
-        return (
-            // <header style={{ background: `${this.state.active ? 'green' : 'blue'}` }}>
-            <header className={this.state.active}>
-                <div className="logo">
-                    logo
+        <header>
+            <div className="logo">
+                logo
                 </div>
-                <input
-                    onChange={this.inputChangeHandler}
-                />
+            <input
+                onChange={props.keywords}
+            />
 
 
-            </header>
-        )
-    }
+        </header>
+    )
+
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -10,18 +10,35 @@ import Footer from './components/footer'
 class App extends Component {
     state = {
         news: JSON,
+        filtered: JSON,
         footerText: "I'm on a sick one FOO"
+    }
+    getKeywords = (event) => {
+        console.log(event.target.value)
+
+        let keywords = event.target.value;
+
+        let filtered = this.state.news.filter((item) => {
+            return item.title.indexOf(keywords) > -1
+        })
+        //setting filtered = filtered so that we can change display and revert back to original list
+        this.setState({
+            filtered: filtered
+        })
     }
 
     render() {
         //creating alias
-        const { news, footerText } = this.state;
+        const { news, footerText, filtered } = this.state;
 
         return (
             <div className='App'>
                 <div className='container'>
-                    <Header />
-                    <NewsList news={news}>
+                    <Header
+                        keywords={this.getKeywords}
+                    // If you put () after getKeywords, the function will run as soon as the app starts
+                    />
+                    <NewsList news={filtered}>
                         {/* Creating children */}
                         <br />
                         <h1>I am bebe</h1>
